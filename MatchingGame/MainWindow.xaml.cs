a97e3699034d8af5fb2d65d7defcd6c5a6ea91d0
88698 <88698@LAPTOP-A7T0LV3K>
2021-12-17 11:08:36 +0800

    Add project files.

---
+++ MatchingGame/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchingGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            //throw new NotImplementedException();
+            List<string> emojiBonbin = new List<string>()
+            {
+               "🐙", "🐙",
+               "🐘","🐘",
+               "🐟","🐟",
+               "🐪", "🐪",
+               "🐋","🐋",
+               "🦕","🦕",
+               "🦘","🦘",
+               "🦔","🦔"
+            };
+
+            Random acak = new Random();
+
+            foreach (TextBlock ngeteks in mainGrid.Children.OfType<TextBlock>())
+            {
+                int pengindeks = acak.Next(emojiBonbin.Count);
+                string emojiNext = emojiBonbin[pengindeks];
+                ngeteks.Text = emojiNext;
+                emojiBonbin.RemoveAt(pengindeks);
+            }
+        }
+    }
+}
